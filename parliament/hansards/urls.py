a12f205dcf1d0766afabd7f8acc98b296a95f818
Michael Mulley <michael@michaelmulley.com>
2010-08-08 14:15:14 -0400

    URL change: 'permalink' -> 'only'

--- parliament/hansards/urls.py
+++ parliament/hansards/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('parliament.hansards.views',
     (r'^(?P<hansard_id>\d+)/$', 'hansard'),
     (r'^(?P<hansard_id>\d+)/(?P<statement_seq>\d+)/$', 'hansard'),
     (r'^(?P<hansard_id>\d+)/(?P<statement_seq>\d+)/twitter/$', 'statement_twitter'),
+    (r'^(?P<hansard_id>\d+)/(?P<statement_seq>\d+)/only/$', 'statement_permalink'),
     (r'^(?P<hansard_id>\d+)/(?P<statement_seq>\d+)/permalink/$', 'statement_permalink'),
     (r'^hansard/(\d+)/local/$', 'hansardcache'),
 )
