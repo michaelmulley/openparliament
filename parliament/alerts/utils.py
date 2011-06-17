9d5abdc534cd448ef00d997d5b6d4c206e3cd2ad
Michael Mulley <michael@michaelmulley.com>
2011-06-17 18:43:40 -0400

    Send an e-mail to subscribers to ex-MPs.

--- parliament/alerts/utils.py
+++ parliament/alerts/utils.py
@@ -6,6 +6,9 @@ from django.core.mail import send_mail
 from parliament.alerts.models import PoliticianAlert
 from parliament.core.templatetags.ours import english_list
 
+import logging
+logger = logging.getLogger(__name__)
+
 def alerts_for_hansard(hansard):
     alerts = PoliticianAlert.public.all()
     alert_set = set([alert.politician_id for alert in alerts])
@@ -40,8 +43,33 @@ def alerts_for_hansard(hansard):
                     message=msg,
                     from_email='alerts@contact.openparliament.ca',
                     recipient_list=[alert.email])
-            except Exception, e:
-                # FIXME logging
-                print "Error sending alert %s" % alert.id
-                print e
-        
+            except Exception as e:
+                logger.error("Error sending alert %s; %r" % (alert.id, e))
+
+def clear_former_mp_alerts(qs=None):
+    from parliament.core.models import ElectedMember
+
+    if qs is None:
+        qs = PoliticianAlert.objects.filter(active=True)
+    bad_alerts = [a for a in qs
+        if not a.politician.current_member]
+    for alert in bad_alerts:
+        riding = alert.politician.latest_member.riding
+        new_politician = ElectedMember.objects.get(riding=riding, end_date__isnull=True).politician
+        t = loader.get_template("alerts/former_mp.txt")
+        c = Context({
+            'politician': alert.politician,
+            'riding': riding,
+            'new_politician': new_politician
+        })
+        msg = t.render(c)
+        subj = u'Your alerts for %s from openparliament.ca' % alert.politician.name
+        try:
+            send_mail(subject=subj,
+                message=msg,
+                from_email='alerts@contact.openparliament.ca',
+                recipient_list=[alert.email])
+            alert.active = False
+            alert.save()
+        except Exception as e:
+            logger.error("Couldn't send mail for alert %s; %r" % (alert.id, e))
