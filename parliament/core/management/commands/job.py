94f4e4c8c9c9123d4b6583196f1efc1eacfbe02b
Michael Mulley <michael@michaelmulley.com>
2011-11-04 13:30:21 -0400

    Allow debugger invocation in jobs module; style tweaks

--- parliament/core/management/commands/job.py
+++ parliament/core/management/commands/job.py
@@ -1,3 +1,4 @@
+from optparse import make_option
 import traceback
 import sys
 
@@ -11,13 +12,24 @@ class Command(BaseCommand):
     help = "Runs a job, which is a no-arguments function in the project's jobs.py"
     args = '[job name]'
     
-    def handle(self, jobname, **kwargs):
+    option_list = BaseCommand.option_list + (
+        make_option('--pdb', action='store_true', dest='pdb', 
+                    help='Launch into Python debugger on exception'),
+    )
+    
+    def handle(self, jobname, **options):
         try:
             getattr(jobs, jobname)()
         except Exception, e:
             try:
-                tb = "\n".join(traceback.format_exception(*(sys.exc_info())))
-                mail_admins("Exception in job %s" % jobname, "\n".join(traceback.format_exception(*(sys.exc_info()))))
+                if options.get('pdb'):
+                    try:
+                        import pudb; pudb.post_mortem()
+                    except ImportError:
+                        import pdb; pdb.post_mortem()
+                else:
+                    tb = "\n".join(traceback.format_exception(*(sys.exc_info())))
+                    mail_admins("Exception in job %s" % jobname, "\n".join(traceback.format_exception(*(sys.exc_info()))))
             except:
                 print tb
             finally:
