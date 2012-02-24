5c7f4d25946efe9aceb87ddebe87e95db6ddf5f8
Michael Mulley <michael@michaelmulley.com>
2012-02-24 16:17:55 -0500

    Missed character in URLconf

--- parliament/committees/urls.py
+++ parliament/committees/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('parliament.committees.views',
     (r'^(?P<committee_id>\d+)/', 'committee_id_redirect'),
     (r'^(?P<slug>[^/]+)/$', 'committee'),
     url(r'^(?P<committee_slug>[^/]+)/(?P<session_id>\d+-\d)/(?P<number>\d+)/$', 'committee_meeting', name='committee_meeting'),
-    url(r'^(?P<committee_slug>[^/]+)/(?P<session_id>\d+-\d)/(?P<number>\d+)/(?P<slug>[a-zA-Z0-9]+)/$', 'committee_meeting', name='committee_meeting'),
+    url(r'^(?P<committee_slug>[^/]+)/(?P<session_id>\d+-\d)/(?P<number>\d+)/(?P<slug>[a-zA-Z0-9-]+)/$', 'committee_meeting', name='committee_meeting'),
 )
