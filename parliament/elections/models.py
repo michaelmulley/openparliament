af327b828bf7aa39f44a945ef8fe041e6a6f6dc6
Arlo Breault <arlolra@gmail.com>
2012-03-23 12:27:46 -0700

    elections

--- parliament/elections/models.py
+++ parliament/elections/models.py
@@ -2,25 +2,27 @@ from decimal import Decimal
 from collections import defaultdict
 
 from django.db import models
+from django.utils.translation import ugettext as _
 
 from parliament.core.models import Politician, Riding, Party, ElectedMember
 
 import logging
 logger = logging.getLogger(__name__)
 
+
 class Election (models.Model):
     date = models.DateField(db_index=True)
     byelection = models.BooleanField()
-    
+
     class Meta:
         ordering = ('-date',)
-    
-    def __unicode__ (self):
+
+    def __unicode__(self):
         if self.byelection:
-            return u"Byelection of %s" % self.date
+            return _(u"Byelection of %s") % self.date
         else:
-            return u"General election of %s" % self.date
-    
+            return _(u"General election of %s") % self.date
+
     def calculate_vote_percentages(self):
         candidacies = self.candidacy_set.all()
         riding_candidacies = defaultdict(list)
@@ -30,9 +32,11 @@ class Election (models.Model):
             riding_votetotals[candidacy.riding_id] += candidacy.votetotal
         for riding in riding_candidacies:
             for candidacy in riding_candidacies[riding]:
-                candidacy.votepercent = (Decimal(candidacy.votetotal) / riding_votetotals[riding]) * 100
+                candidacy.votepercent = (
+                    Decimal(candidacy.votetotal) / riding_votetotals[riding]
+                ) * 100
                 candidacy.save()
-                
+
     def label_winners(self):
         """Sets the elected boolean on this election's Candidacies"""
         candidacies = self.candidacy_set.all()
@@ -45,21 +49,22 @@ class Election (models.Model):
             winner.elected = True
             winner.save()
         candidacies.filter(elected=None).update(elected=False)
-        
+
     def create_members(self, session=None):
         for candidacy in self.candidacy_set.filter(elected=True):
             candidacy.create_member(session)
-                
+
+
 class CandidacyManager(models.Manager):
-    
+
     def create_from_name(self, first_name, last_name, riding, party, election,
         votetotal, elected, votepercent=None, occupation='', interactive=True):
         """Create a Candidacy based on a candidate's name; checks for prior
         Politicians representing the same person.
-        
+
         first_name and last_name are strings; remaining arguments are as in
         the Candidacy model"""
-        
+
         candidate = None
         fullname = u' '.join((first_name, last_name))
         candidates = Politician.objects.filter_by_name(fullname)
@@ -68,26 +73,49 @@ class CandidacyManager(models.Manager):
             # Does the candidate have many given names?
             if first_name.strip().count(' ') >= 1:
                 minifirst = first_name.strip().split(' ')[0]
-                candidates = Politician.objects.filter_by_name("%s %s" % (minifirst, last_name))
+                candidates = Politician.objects.filter_by_name(
+                    "%s %s" % (minifirst, last_name)
+                )
         # Then, evaluate the possibilities in the list
         for posscand in candidates:
-            # You're only a match if you've run for office for the same party in the same province
-            match = ElectedMember.objects.filter(riding__province=riding.province, party=party, politician=posscand).count() >= 1 or Candidacy.objects.filter(riding__province=riding.province, party=party, candidate=posscand).count() >= 1
+            # You're only a match if you've run for office for
+            #the same party in the same province
+            match = ElectedMember.objects.filter(
+                riding__province=riding.province,
+                party=party,
+                politician=posscand
+            ).count() >= 1 or Candidacy.objects.filter(
+                riding__province=riding.province,
+                party=party,
+                candidate=posscand
+            ).count() >= 1
             if match:
                 if candidate is not None:
                     if interactive:
-                        print "Please enter Politician ID for %r (%r)" % (fullname, riding.name)
-                        candidate = Politician.objects.get(pk=raw_input().strip())
+                        print _("Please enter Politician ID for " \
+                            "%(fullname)r (%(riding)r)") % {
+                            fullname: fullname,
+                            riding: riding.name
+                        }
+                        candidate = Politician.objects.get(
+                            pk=raw_input().strip()
+                        )
                         break
                     else:
                         raise Politician.MultipleObjectsReturned(
-                            "Could not disambiguate among existing candidates for %s" % fullname)
+                            _("Could not disambiguate among existing " \
+                            "candidates for %s") % fullname
+                        )
                 candidate = posscand
-                    
+
         if candidate is None:
-            candidate = Politician(name=fullname, name_given=first_name, name_family=last_name)
+            candidate = Politician(
+                name=fullname,
+                name_given=first_name,
+                name_family=last_name
+            )
             candidate.save()
-            
+
         return self.create(
             candidate=candidate,
             riding=riding,
@@ -98,7 +126,7 @@ class CandidacyManager(models.Manager):
             votepercent=votepercent,
             occupation=occupation
         )
-        
+
 
 class Candidacy (models.Model):
     candidate = models.ForeignKey(Politician)
@@ -107,24 +135,32 @@ class Candidacy (models.Model):
     election = models.ForeignKey(Election)
     occupation = models.CharField(max_length=100, blank=True)
     votetotal = models.IntegerField(blank=True, null=True)
-    votepercent = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    votepercent = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
     elected = models.NullBooleanField(blank=True, null=True)
-    
+
     objects = CandidacyManager()
-    
+
     class Meta:
-        verbose_name_plural = 'Candidacies'
-        
+        verbose_name_plural = _('Candidacies')
+
     def create_member(self, session=None):
         """Creates an ElectedMember for a winning candidate"""
         if not self.elected:
             return False
         try:
-            member = ElectedMember.objects\
-                .filter(models.Q(end_date__isnull=True) | models.Q(end_date=self.election.date))\
-                .get(politician=self.candidate,
+            member = ElectedMember.objects.filter(
+                models.Q(end_date__isnull=True)
+                | models.Q(end_date=self.election.date)
+            ).get(
+                politician=self.candidate,
                 riding=self.riding,
-                party=self.party)
+                party=self.party
+            )
             member.end_date = None
             member.save()
         except ElectedMember.DoesNotExist:
@@ -137,6 +173,12 @@ class Candidacy (models.Model):
         if session:
             member.sessions.add(session)
         return member
-    
-    def __unicode__ (self):
-        return u"%s (%s) was a candidate in %s in the %s" % (self.candidate, self.party, self.riding, self.election)
+
+    def __unicode__(self):
+        return _(u"%(candidate)s (%(party)s) was a candidate in " \
+            "%(riding)s in the %(election)s") % {
+            candidate: self.candidate,
+            party: self.party,
+            riding: self.riding,
+            election: self.election
+        }
