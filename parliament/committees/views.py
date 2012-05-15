2aa7082928e3296686c075704431c79cadb000a9
Michael Mulley <michael@michaelmulley.com>
2012-05-14 20:23:51 -0400

    Show recent studies on committee list

--- parliament/committees/views.py
+++ parliament/committees/views.py
@@ -2,16 +2,23 @@ from django.http import HttpResponse, HttpResponsePermanentRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader, RequestContext
 
-from parliament.committees.models import Committee, CommitteeMeeting
+from parliament.committees.models import Committee, CommitteeMeeting, CommitteeActivity
 from parliament.core.models import Session
 from parliament.hansards.views import document_view
 
 def committee_list(request):
     committees = Committee.objects.filter(sessions=Session.objects.current(),
         parent__isnull=True)
+
+    recent_meetings = CommitteeMeeting.objects.order_by('-date')[:20]
+    recent_studies = CommitteeActivity.objects.filter(
+        study=True,
+        committeemeeting__in=list(recent_meetings.values_list('id', flat=True))
+    ).distinct()
     return render(request, "committees/committee_list.html", {
         'object_list': committees,
         'title': 'House Committees',
+        'recent_studies': recent_studies
     })
 
 def committee_id_redirect(request, committee_id):
