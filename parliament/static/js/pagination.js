80e67dfcfddcd3ddf9237fd69184cdbd9f03edb9
Michael Mulley <michael@michaelmulley.com>
2012-05-27 12:55:10 -0400

    Tell Analytics about ajax navigation

--- parliament/static/js/pagination.js
+++ parliament/static/js/pagination.js
@@ -11,10 +11,10 @@ $(function() {
 
     $(window).bind('statechange', function(e) {
         var newStateURL = History.getState().url;
-        newStateURL += (newStateURL.indexOf('?') === -1 ? '?' : '&') + 'partial=1';
         var $paginated = $('#paginated');
         $paginated.find('.pagination').addClass('loading');
-        $paginated.load(newStateURL, '', function() {
+        $paginated.load(newStateURL + (newStateURL.indexOf('?') === -1 ? '?' : '&') + 'partial=1',
+            '', function() {
             $(document).trigger('contentLoad');
             $paginated.css({opacity: 1.0});
             var scrollDown = Boolean($(document).scrollTop() > $paginated.offset().top);
@@ -27,6 +27,12 @@ $(function() {
                 $('html,body').animate({scrollTop: $paginated.offset().top - 15});
             }
         }).css({opacity: 0.6});
+
+        // Tell Google Analytics about the new hit
+        if (typeof window._gaq !== 'undefined') {
+            var relativeURL = '/' + newStateURL.replace(History.getRootUrl(), '');
+            window._gaq.push(['_trackPageview', relativeURL]);
+        }
     });
 
     var hash = History.getHash();
