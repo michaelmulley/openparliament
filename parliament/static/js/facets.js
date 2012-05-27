782040c0f8c9fa76801925c6f8a2313f46cc8f1d
Michael Mulley <michael@michaelmulley.com>
2012-05-27 17:25:12 -0400

    Search style tweaks

--- parliament/static/js/facets.js
+++ parliament/static/js/facets.js
@@ -1,24 +1,5 @@
 (function(){
 
-//    var facetTemplate = _.template(
-//        "<% for (var i = 0; i < data.length; i++) { %>" +
-//            "<ul><% for (var j = 0; j < data[i].values.length; j += 2) { %>" +
-//                '<li><a href="#"><%- data[i].values[j] %></a> <%- data[i].values[j+1]%></li>' +
-//            "<% } %></ul>" +
-//        "<% } %>"
-//    );
-
-//    var facetTemplate = _.template(
-//        "<table><% for (var row = 0; row < maxFacetResults; row++) { %>" +
-//            "<tr><% for (var ftype = 0; ftype < data.length; ftype++) {%>" +
-//                "<td><% if (data[ftype].values[row*2]) { %>" +
-//                    '<div class="barbg" style="width: <%= Math.round((data[ftype].values[(row*2)+1]/data[ftype].max) * 90) %>%"></div>' +
-//                    '<div class="label"><a href="#"><%- data[ftype].values[row*2] %></a> <em><%- data[ftype].values[(row*2)+1] %></em></div>' +
-//                '</td><% } %>' +
-//            "<% } %></tr>" +
-//        "<% } %></table>"
-//    );
-
     var facetTemplate = _.template(
         "<% for (var ftype = 0; ftype < data.length; ftype++) { if (data[ftype].values.length > 1) { %>" +
             '<div class="facetgroup"><% for (var row = 0; row < data[ftype].values.length; row++) { %>' +
@@ -36,7 +17,7 @@
     OP.FacetWidget = function(opts) {
         opts = opts || {};
         _.defaults(opts, {
-            collapseAfter: 3
+            collapseAfter: 4
         });
         _.extend(this, opts);
         this.$el = $('<div class="facetwidget"></div>');
