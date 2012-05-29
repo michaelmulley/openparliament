1aaced816ea206a85c0a3cf99915f09af2517e14
Michael Mulley <michael@michaelmulley.com>
2012-05-29 16:43:12 -0400

    Add party/province to politician search index

--- parliament/core/search_indexes.py
+++ parliament/core/search_indexes.py
@@ -8,6 +8,8 @@ class PolIndex(SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     boosted = indexes.CharField(use_template=True, stored=False)
     politician = indexes.CharField(model_attr='name', indexed=False)
+    party = indexes.CharField(model_attr='latest_member__party__short_name')
+    province = indexes.CharField(model_attr='latest_member__riding__province')
     url = indexes.CharField(model_attr='get_absolute_url', indexed=False)
     #dob = indexes.DateTimeField(model_attr='dob')
     
