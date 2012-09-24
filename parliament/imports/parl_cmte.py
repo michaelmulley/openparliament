6149a980c59dce859262c977da319840551b44f9
Michael Mulley <michael@michaelmulley.com>
2012-09-24 18:33:37 -0400

    Downgrade an error

--- parliament/imports/parl_cmte.py
+++ parliament/imports/parl_cmte.py
@@ -193,7 +193,7 @@ def get_activity_by_url(activity_url):
 
     if CommitteeActivityInSession.objects.exclude(source_id=activity_id).filter(
             session=session, activity=activity).exists():
-        logger.error("Apparent duplicate activity ID for %s %s %s: %s" %
+        logger.warning("Apparent duplicate activity ID for %s %s %s: %s" %
                      (activity, activity.committee, session, activity_id))
         return activity
     
