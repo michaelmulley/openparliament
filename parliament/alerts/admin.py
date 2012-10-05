2e4c668ade56a7b3f9da4165d3fbe8e6b87ace78
Michael Mulley <michael@michaelmulley.com>
2012-10-05 13:23:16 -0400

    Alerts admin tweaks

--- parliament/alerts/admin.py
+++ parliament/alerts/admin.py
@@ -14,6 +14,7 @@ class TopicAdmin(admin.ModelAdmin):
 
     list_display = ['query', 'created', 'last_found']
     search_fields = ['query']
+    ordering = ['-created']
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
@@ -21,6 +22,7 @@ class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ['user', 'topic', 'active', 'created', 'last_sent']
     search_fields = ['user__email']
     list_filter = ['active', 'created', 'last_sent']
+    ordering = ['-created']
 
 admin.site.register(Topic, TopicAdmin)
 admin.site.register(Subscription, SubscriptionAdmin)
