f635409a281164447a188218188fb64b2d26993c
Michael Mulley <michael@michaelmulley.com>
2013-03-15 20:15:24 -0400

    Tweak default paths

--- parliament/default_settings.py
+++ parliament/default_settings.py
@@ -39,7 +39,7 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJ_ROOT, 'media/')
+MEDIA_ROOT = os.path.realpath(os.path.join(PROJ_ROOT, '..', '..', 'mediafiles'))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -47,7 +47,7 @@ MEDIA_ROOT = os.path.join(PROJ_ROOT, 'media/')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [os.path.join(PROJ_ROOT, 'static')]
-STATIC_ROOT = os.path.join(PROJ_ROOT, '..', 'collected_static')
+STATIC_ROOT = os.path.realpath(os.path.join(PROJ_ROOT, '..', '..', 'staticfiles'))
 STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = [
