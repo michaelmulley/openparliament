45382b0630cd8502aae8292481b9288709bfcf2f
Michael Mulley <michael@michaelmulley.com>
2016-01-19 09:36:26 -0500

    update statement admin

--- parliament/hansards/admin.py
+++ parliament/hansards/admin.py
@@ -5,6 +5,11 @@ from parliament.hansards.models import *
 class DocumentOptions(admin.ModelAdmin):
     list_display=('number', 'date', 'session', 'document_type', 'committeemeeting')
     list_filter=('document_type', 'session', 'date', 'multilingual', 'public')
+
+class StatementOptions(admin.ModelAdmin):
+    list_display = ('politician', 'time', 'document', 'wordcount', 'procedural')
+    list_filter = ('time',)
+    raw_id_fields = ('document', 'member', 'politician', 'bills', 'mentioned_politicians')
     
 admin.site.register(Document, DocumentOptions)
-admin.site.register(Statement)
+admin.site.register(Statement, StatementOptions)
