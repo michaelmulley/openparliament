fcb3f124756b92c3901f2e0fe0444d6fc8ff7b1a
Michael Mulley <michael@michaelmulley.com>
2016-09-06 17:05:37 -0400

    Tweak text-analysis admin

--- parliament/text_analysis/admin.py
+++ parliament/text_analysis/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from parliament.text_analysis.models import TextAnalysis
 
-admin.site.register(TextAnalysis)
+class TextAnalysisOptions(admin.ModelAdmin):
+    search_fields = ('key',)
+    list_display = ['key', 'lang', 'updated']
+
+admin.site.register(TextAnalysis, TextAnalysisOptions)
