0e5a562ac2a7cb2575a3380feb5f289315b9fa98
Michael Mulley <michael@michaelmulley.com>
2017-02-25 20:58:32 -0500

    Django 1.10 compat: use argparse instead of optparse in command

--- parliament/core/management/commands/job.py
+++ parliament/core/management/commands/job.py
@@ -1,8 +1,7 @@
-from optparse import make_option
 import traceback
 import sys
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.core.mail import mail_admins
 
 try:
@@ -16,10 +15,10 @@ class Command(BaseCommand):
     help = "Runs a job, which is a no-arguments function in the project's jobs.py"
     args = '[job name]'
     
-    option_list = BaseCommand.option_list + (
-        make_option('--pdb', action='store_true', dest='pdb', 
-                    help='Launch into Python debugger on exception'),
-    )
+    def add_arguments(self, parser):
+        parser.add_argument('jobname', type=str)
+        parser.add_argument('--pdb', action='store_true', dest='pdb',
+            help='Launch into Python debugger on exception')
     
     def handle(self, jobname, **options):
         try:
