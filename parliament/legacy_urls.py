2b3e281c228a4efa9483362f10eac74ce4da6178
Michael Mulley <michael@michaelmulley.com>
2017-03-03 14:36:33 -0500

    Fix a couple of redirect URLs

--- parliament/legacy_urls.py
+++ parliament/legacy_urls.py
@@ -4,8 +4,8 @@ from parliament.core.utils import redir_view
 from parliament.hansards.redirect_views import hansard_redirect
 
 urlpatterns = [
-    url(r'^hansards/$', redir_view('parliament.hansards.views.index')),
-    url(r'^hansards/year/(?P<year>\d{4})/$', redir_view('parliament.hansards.views.by_year')),
+    url(r'^hansards/$', redir_view('debates')),
+    url(r'^hansards/year/(?P<year>\d{4})/$', redir_view('debates_by_year')),
     url(r'^hansards/(?P<hansard_id>\d+)/$', hansard_redirect),
     url(r'^hansards/(?P<hansard_date>[0-9-]+)/$', hansard_redirect),
     url(r'^hansards/(?P<hansard_id>\d+)/(?P<sequence>\d+)/$', hansard_redirect),
