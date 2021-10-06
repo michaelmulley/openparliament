1f35ec7b1b97b0e6c272aaf0e2fbca24d2a94b41
Michael Mulley <michael@michaelmulley.com>
2021-10-06 19:12:17 -0400

    improve Candidacy admin

--- parliament/elections/admin.py
+++ parliament/elections/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from parliament.elections.models import *
 
+class CandidacyOptions (admin.ModelAdmin):
+
+    list_display = ('candidate', 'riding', 'party', 'election', 'elected', 'votepercent')
+    search_fields = ('candidate__name', 'riding__name_en', 'party__name_en')
+    list_filter = ('party', 'election', 'elected')
+
 admin.site.register(Election)
-admin.site.register(Candidacy)
+admin.site.register(Candidacy, CandidacyOptions)
