85286264accad0a633de6650859ea6012d757020
Michael Mulley <michael@michaelmulley.com>
2024-05-24 10:41:15 -0400

    remove old sentry sdk

--- parliament/static/js/core.js
+++ parliament/static/js/core.js
@@ -141,16 +141,6 @@ $('.overflowtip').overflowtip();
 $(document).foundation();
 
 $(function() {
-	if (window.Raven) {
-	    Raven.config(
-	        'https://b5fd50dac5844b9a872b9fb5718ae980@sentry.io/113972',
-	        {
-	            whitelistUrls: [ /openparliament\.ca/ ],
-	            ignoreUrls: [  /extensions\//i, /^chrome:\/\//i ]
-	        }
-	    ).install();
-	}
-
 	$('#navbar-buttons-search').click(function(e) {
 		e.preventDefault();
 		var $searchbar = $('#navbar-search');
