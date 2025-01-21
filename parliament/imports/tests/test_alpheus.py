3054883d1fb8dce25d136ba7465ebbbc9e6bfc00
Michael Mulley <michael@michaelmulley.com>
2025-01-21 13:37:19 -0500

    add integration tests for alpheus

---
+++ parliament/imports/tests/test_alpheus.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+# from unittest import TestCase
+from django.test import TestCase
+
+from parliament.imports import alpheus
+
+def get_html(xml_path: Path) -> str:
+    doc = alpheus.parse_bytes(xml_path.read_bytes())
+    return doc.as_html()
+
+def write_example_html(xml_path: Path):
+    assert xml_path.exists()
+    html_path = xml_path.parent / f'{xml_path.stem}.html'
+    html = get_html(xml_path)
+    html_path.write_text(html, encoding='utf8')
+
+def overwrite_all_example_html():
+    examples_dir = Path(__file__).parent / 'alpheus_examples'
+    for example_xml_path in examples_dir.glob('*.xml'):
+        write_example_html(example_xml_path)
+
+class AlpheusIntegrationTests(TestCase):
+
+    def test_examples(self):
+        examples_dir = Path(__file__).parent / 'alpheus_examples'
+        for example_xml_path in examples_dir.glob('*.xml'):
+            example_html_path = examples_dir / f'{example_xml_path.stem}.html'
+            assert example_html_path.exists(), f"Missing {example_html_path}"
+            output_html = get_html(example_xml_path)
+            self.assertMultiLineEqual(output_html,
+                                      example_html_path.read_text(encoding='utf8'),
+                                      msg=f"Comparing HTML output of {example_xml_path.stem}")
+            
